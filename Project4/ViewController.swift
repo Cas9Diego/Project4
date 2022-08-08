c9d2d4484c61dc4d9976b601f900e2fd0754cbfa
Cas9Diego <DaCastro@outlook.com>
2022-08-08 00:10:21 -0500

    Basic browser that allows the user to access apple.com and google.com

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -6,12 +6,40 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action:#selector (openTapped))
+        
+        let url = URL (string: "https://www.google.com/")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    @objc func openTapped () {
+        let ac = UIAlertController(title: "Open Page", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "google.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present (ac, animated: true)
+    }
+    
+    func openPage (action: UIAlertAction) {
+        let url = URL(string: "https://"+action.title!)!
+        webView.load(URLRequest(url: url))
     }
 
 
